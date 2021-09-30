b1ea6f1a7d1102026d25e001ca9ae6e378a59111
Davidson Bpe <davidsomixer@gmail.com>
2021-09-30 13:20:40 -0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,26 +1,4 @@
 <?php
-function json2xml( $domNode ) {
-    foreach( $domNode -> childNodes as $node) {
-     if ( $node -> hasChildNodes() ) { json2xml( $node ); }
-     else {
-      if ( $domNode -> hasAttributes() && strlen( $domNode -> nodeValue ) ) {
-       $domNode -> setAttribute( "nodeValue", $node -> textContent );
-       $node -> nodeValue = "";
-      }
-     }
-    }
-   }
-  
-   function jsonOut( $file ) {
-    $dom = new DOMDocument();
-    $dom -> loadXML( file_get_contents( $file ) );
-    json2xml( $dom );
-    header( 'Content-Type: application/json' );
-    return str_replace( "@", "", json_encode( simplexml_load_string( $dom -> saveXML() ), JSON_PRETTY_PRINT ) );
-   }
-  
-   $output = jsonOut( 'https://feeds.feedburner.com/Davidsonbpe' );
-  
-   echo( $output );
+
 
 ?>
